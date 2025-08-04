06bdc1492776ae48408f0aa48392f96d8531f089
Raahim Nadeem <muhammad.nadeem2@careem.com>
2025-08-05 03:31:16 +0500

    Remove the gradient overlays

--- components/ClientLogosMarquee.tsx
+++ components/ClientLogosMarquee.tsx
@@ -178,9 +178,7 @@ export default function ClientLogosMarquee() {
         </motion.div>
       </div>
 
-      {/* Gradient overlays for smooth fade effect */}
-      <div className="absolute left-0 top-0 bottom-0 w-20 bg-gradient-to-r from-white to-transparent z-10" />
-      <div className="absolute right-0 top-0 bottom-0 w-20 bg-gradient-to-l from-white to-transparent z-10" />
+
     </section>
   );
 } 
